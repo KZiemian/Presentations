2f3716f6f01a2b494964690a99a408eed7ea4e72
Kamil Ziemian <kziemianfvt@gmail.com>
2025-03-14 14:31:59 +0100

    Systemy operacyjne, 09

---
+++ Computer-science-Presentations/Systemy-operacyjne-Prezentacje/Materiały-do-prezentacji/generate-files-for-regexp-B.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+echo "Zawartość pliku „Data-01.dat”." >> Data-01.dat
+echo "Zawartość pliku „Data-02.dat”." >> Data-02.dat
+echo "Zawartość pliku „Data-03.dat”." >> Data-03.dat
+echo "Zawartość pliku „Data-04.dat”." >> Data-04.dat
+echo "Zawartość pliku „Data-05.dat”." >> Data-05.dat
+
+echo "Zawartość pliku „File-01.txt”." >> File-01.txt
+echo "Zawartość pliku „File-02.txt”." >> File-02.txt
+echo "Zawartość pliku „File-03.txt”." >> File-03.txt
+echo "Zawartość pliku „File-04.txt”." >> File-04.txt
+echo "Zawartość pliku „File-05.txt”." >> File-05.txt
+echo "Zawartość pliku „File-06.txt”." >> File-06.txt
+echo "Zawartość pliku „File-07.txt”." >> File-07.txt
+echo "Zawartość pliku „File-08.txt”." >> File-08.txt
+echo "Zawartość pliku „File-09.txt”." >> File-09.txt
+echo "Zawartość pliku „File-10.txt”." >> File-10.txt
+
+echo "Zawartość pliku „File-01-A.txt”." >> File-01-A.txt
+echo "Zawartość pliku „File-01-a.txt”." >> File-01-a.txt
+echo "Zawartość pliku „File-01-B.txt”." >> File-01-B.txt
+echo "Zawartość pliku „File-01-b.txt”." >> File-01-b.txt
+echo "Zawartość pliku „File-01-C.txt”." >> File-01-C.txt
+echo "Zawartość pliku „File-01-c.txt”." >> File-01-c.txt
+echo "Zawartość pliku „File-01-D.txt”." >> File-01-D.txt
+echo "Zawartość pliku „File-01-d.txt”." >> File-01-d.txt
+echo "Zawartość pliku „File-01-E.txt”." >> File-01-E.txt
+echo "Zawartość pliku „File-01-e.txt”." >> File-01-e.txt
+
+echo "Zawartość pliku „File-02-A.txt”." >> File-02-A.txt
+echo "Zawartość pliku „File-02-B.txt”." >> File-02-B.txt
+echo "Zawartość pliku „File-02-C.txt”." >> File-02-C.txt
+echo "Zawartość pliku „File-02-D.txt”." >> File-02-D.txt
+echo "Zawartość pliku „File-02-E.txt”." >> File-02-E.txt
+
+echo "Zawartość pliku „File-03-A.txt”." >> File-03-A.txt
+echo "Zawartość pliku „File-03-B.txt”." >> File-03-B.txt
+echo "Zawartość pliku „File-03-C.txt”." >> File-03-C.txt
+echo "Zawartość pliku „File-03-D.txt”." >> File-03-D.txt
+echo "Zawartość pliku „File-03-E.txt”." >> File-03-E.txt
+
+echo "Zawartość pliku „File-04-A.txt”." >> File-04-A.txt
+echo "Zawartość pliku „File-04-B.txt”." >> File-04-B.txt
+echo "Zawartość pliku „File-04-C.txt”." >> File-04-C.txt
+echo "Zawartość pliku „File-04-D.txt”." >> File-04-D.txt
+echo "Zawartość pliku „File-04-E.txt”." >> File-04-E.txt
+
+echo "Zawartość pliku „File-05-A.txt”." >> File-05-A.txt
+echo "Zawartość pliku „File-05-B.txt”." >> File-05-B.txt
+echo "Zawartość pliku „File-05-C.txt”." >> File-05-C.txt
+echo "Zawartość pliku „File-05-D.txt”." >> File-05-D.txt
+echo "Zawartość pliku „File-05-E.txt”." >> File-05-E.txt
+
+echo "Zawartość pliku „Jakis-plik.txt”." >> Jakis-plik.txt
