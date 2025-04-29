c3f2fa341cff4966f8c5390d57c2d5857c836a0d
Kamil Ziemian <kziemianfvt@gmail.com>
2025-04-30 00:18:54 +0200

    Systemy opercyjne, 18

---
+++ Computer-science-Presentations/Programming-in-Java-Presentations/Materiały-do-prezentacji/SO-Ćwiczenie-01-Pliki-i-katalogi/SO-remove-files-for-excercise-01.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+rm Data-01.dat
+rm Data-02.dat
+rm Data-03.dat
+rm Data-04.dat
+rm Data-05.dat
+rm Data-06.dat
+rm Data-07.dat
+rm Data-08.dat
+rm Data-09.dat
+rm Data-10.dat
+
+rm File-01.txt
+rm File-02.txt
+rm File-03.txt
+rm File-04.txt
+rm File-05.txt
+rm File-06.txt
+rm File-07.txt
+rm File-08.txt
+rm File-09.txt
+rm File-10.txt
+
+rm File-01-A.txt
+rm File-01-a.txt
+rm File-01-B.txt
+rm File-01-b.txt
+rm File-01-C.txt
+rm File-01-c.txt
+rm File-01-D.txt
+rm File-01-d.txt
+rm File-01-E.txt
+rm File-01-e.txt
+
+rm File-02-A.txt
+rm File-02-B.txt
+rm File-02-C.txt
+rm File-02-D.txt
+rm File-02-E.txt
+
+rm File-03-A.txt
+rm File-03-B.txt
+rm File-03-C.txt
+rm File-03-D.txt
+rm File-03-E.txt
+
+rm File-04-A.txt
+rm File-04-B.txt
+rm File-04-C.txt
+rm File-04-D.txt
+rm File-04-E.txt
+
+rm File-05-A.txt
+rm File-05-B.txt
+rm File-05-C.txt
+rm File-05-D.txt
+rm File-05-E.txt
+
+rm Normal-file.txt
