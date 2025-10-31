cedb936f9f43678ae63c26a9ae855c17a4d4a134
Kamil Ziemian <kziemianfvt@gmail.com>
2025-10-31 19:59:51 +0100

    Podstawy informatyki, 40

--- Computer-science-Presentations/Podstawy-informatyki-ETC-Prezentacje/Materiały-do-kursu/Wprowadzenie-do-programowania-w-C/PI-05-A-Zmienne-boolowskie-w-Go.go
+++ Computer-science-Presentations/Podstawy-informatyki-ETC-Prezentacje/Materiały-do-kursu/Wprowadzenie-do-programowania-w-C/PI-05-A-Zmienne-boolowskie-w-Go.go
@@ -2,11 +2,18 @@ package main
 
 import "fmt"
 
+// Jeśli mamy zainstalowany program do uruchamiania kodu napisanego w Go,
+// to plik ten możemy uruchomić z poziomu powłoki poleceniem
+// go run PI-05-A-Zmienne-boolowskie-w-Go.go
+
 func main() {
+	// Tworzymy zmienną boolowską „boolVar1” i nadajemy jej
+	// wartość „false”.
 	var boolVar1 bool = true
 
 	fmt.Printf("boolVar1 == %v.\n", boolVar1)
 
+	// Zmieniamy
 	boolVar1 = false
 
 	fmt.Printf("boolVar1 == %v.\n", boolVar1)
